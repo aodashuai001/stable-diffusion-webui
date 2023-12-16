9862c910535921bce00f67e5b5723c9bcd3e698b
aodashuai001 <281621528@qq.com>
2023-12-16 22:23:23 +0800

    1

--- webui-user.bat
+++ webui-user.bat
@@ -1,10 +1,10 @@
 @echo off
 
-# set PYTHON=e:\Python\Python310\python.exe
+@REM # set PYTHON=e:\Python\Python310\python.exe
 # set GIT=
 # set VENV_DIR=
 @REM set COMMANDLINE_ARGS=--ngrok 2THBdu0Ue0aurmJIeLP9RuF1YRn_7pjYQbhzHTWRn7LuvST7B  --xformers --no-half --no-half-vae --disable-nan-check --listen --no-gradio-queue
 @REM  --share --gradio-auth tuling:tuling
-set COMMANDLINE_ARGS=--xformers --no-half --no-half-vae --disable-nan-check --enable-insecure-extension-access --listen
+set COMMANDLINE_ARGS=--xformers --no-half --no-half-vae --disable-nan-check --enable-insecure-extension-access --listen --reinstall-torch --profiles dev
 
 call webui.bat
